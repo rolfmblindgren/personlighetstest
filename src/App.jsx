e0fe987f5feef2d515fb758ef49ba82ea8172d93
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-07-28 21:39:13 +0200

    flytt bildene

--- src/App.jsx
+++ src/App.jsx
@@ -62,24 +62,24 @@ function App() {
 <picture>
   <source
     srcSet="
-      ./bilder/zahlenzauberer-480.webp 480w,
-      ./bilder/zahlenzauberer-640.webp 640w,
-      ./bilder/zahlenzauberer-800.webp 800w,
-      ./bilder/zahlenzauberer-1280.webp 1280w,
-      ./bilder/zahlenzauberer-1920.webp 1920w"
+      /bilder/zahlenzauberer-480.webp 480w,
+      /bilder/zahlenzauberer-640.webp 640w,
+      /bilder/zahlenzauberer-800.webp 800w,
+      /bilder/zahlenzauberer-1280.webp 1280w,
+      /bilder/zahlenzauberer-1920.webp 1920w"
     type="image/webp"
     sizes="(max-width: 600px) 100vw, (max-width: 1200px) 80vw, 60vw"
   />
   <source
     srcSet="
-      ./bilder/zahlenzauberer-480.png 480w,
-      ./bilder/zahlenzauberer-1280.png 1280w,
-      ./bilder/zahlenzauberer-1920.png 1920w"
+      /bilder/zahlenzauberer-480.png 480w,
+      /bilder/zahlenzauberer-1280.png 1280w,
+      /bilder/zahlenzauberer-1920.png 1920w"
     type="image/png"
     sizes="(max-width: 600px) 100vw, (max-width: 1200px) 80vw, 60vw"
   />
   <img
-    src="./bilder/zahlenzauberer-1280.png"
+    src="/bilder/zahlenzauberer-1280.png"
     alt="Personer i forskjellige aktiviteter"
     style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
   />
