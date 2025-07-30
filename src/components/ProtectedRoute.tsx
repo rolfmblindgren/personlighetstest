ca8a3da28be1f6b9b65e743c34ff148b5f27bb08
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-07-30 21:22:45 +0200

    typo

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,7 +7,7 @@ function isTokenValid(): boolean {
   if (!token) return false
 
   try {
-    const decoded: any = jwt_decode(token)
+    const decoded: any = jwtDecode(token)
     const now = Date.now() / 1000
     return decoded.exp && decoded.exp > now
   } catch {
