3e3c16c21ed1c14dd164113004ee488b6714d58d
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-07-30 20:56:18 +0200

    legg til ei fil til

---
+++ src/auth.tsx
@@ -0,0 +1,14 @@
+import jwt_decode from 'jwt-decode'
+
+export function isTokenValid(): boolean {
+  const token = localStorage.getItem('token')
+  if (!token) return false
+
+  try {
+    const decoded: any = jwt_decode(token)
+    const now = Date.now() / 1000  // sekunder
+    return decoded.exp && decoded.exp > now
+  } catch {
+    return false
+  }
+}
