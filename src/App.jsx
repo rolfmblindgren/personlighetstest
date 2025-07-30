7ec2db44877d5d288d2a102e1d79f5c98890e0fb
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-07-30 17:12:33 +0200

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
-// import Kontrollpanel from './kontrollpanel'
-import Dashboard from './kontrollpanel2'
+import Kontrollpanel from './kontrollpanel'
+// import Dashboard from './kontrollpanel2'
 
 function App() {
   console.log("App.jsx laster")
@@ -13,8 +13,8 @@ function App() {
         <Routes>
 	  <Route path="*" element={<div>Ingen rute matchet</div>} />
           <Route path="/" element={<LandingPage />} />
-          <Route path="/kontrollpanel2" element={<Dashboard />} />
-//          <Route path="/kontrollpanel" element={<Kontrollpanel />} />
+//          <Route path="/kontrollpanel2" element={<Dashboard />} />
+          <Route path="/kontrollpanel" element={<Kontrollpanel />} />
         </Routes>
       </Router>
     </HelmetProvider>
