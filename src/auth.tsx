0a00e791a2295f8c390db06b273870441ed67d0b
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-07-30 21:00:22 +0200

    fikse på typo

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,11 +1,11 @@
-import { jwt_decode } from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 
 export function isTokenValid(): boolean {
   const token = localStorage.getItem('token')
   if (!token) return false
 
   try {
-    const decoded: any = jwt_decode(token)
+    const decoded: any = jwtDecode(token)
     const now = Date.now() / 1000  // sekunder
     return decoded.exp && decoded.exp > now
   } catch {
