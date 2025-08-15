e8dbb212d3aed7a7c649ab44c9b55a88ec9e7426
Rolf Marvin Bøe Lindgren <rolf@grendel.no>
2025-08-15 18:32:36 +0200

    turkis tailwind

---
+++ src/tailwind.config.cs
@@ -0,0 +1,29 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tilpass til mappestrukturen din
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        turkis: {
+          50:  '#ebfefc',
+          100: '#c6fdf7',
+          200: '#92fbf0',
+          300: '#5cf7e7',
+          400: '#2ceedd',
+          500: '#15d5c4', // hovedfargen
+          600: '#0da89b',
+          700: '#0c8178',
+          800: '#0e635c',
+          900: '#0e4f4a',
+        },
+      },
+      borderRadius: {
+        'xl': '1rem', // ekstra myke hjørner
+      },
+    },
+  },
+  plugins: [],
+}
